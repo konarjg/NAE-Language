6f001c9da95efa0bd086ce83236e9b02da09c719
konarjg <konarskikrzysztof1@gmail.com>
2022-12-27 15:19:36 +0100

    Added built in libraries Math and Random (#3)

---
+++ Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Library
+{
+    public string Name;
+    public string[] Functions;
+    public Variable[] Constants;
+    public bool Imported;
+
+    public Library(string name)
+    {
+        Name = name;
+    }
+
+    public abstract dynamic ExecuteFunction(string name, ref Stack<string> stack);
+}
